9fc4047f3480fce7f4dbe44e90d02263baad0738
Videsh Arivazhagan <arivvid27@outlook.com>
2021-01-30 21:42:08 +0000

    I have made some improvements

--- main.py
+++ main.py
@@ -1,44 +1,97 @@
-operation = input('''
+import math
+import replit
+from time import sleep
+from termcolor import colored
+colors = "red", "yellow", "cyan", "magenta", "green"
+
+def clear():
+    replit.clear()
+
+while True:
+	operation = input('''
 ~~~~~COMMANDS~~~~~
-[a]dd
-[s]ubtract
-[m]ultiply
-[d]ivide
-[i]nteger division
-[mo]dulo
-[q]uit
+[add]
+[subtract]
+[multiply]
+[divide]
+[integer division]
+[modulo]
+[power of]
+[square root]
+[expressions]
+[quit]
 >>> ''')
-if operation == 'a':
-	num1_add = int(input('num1 > '))
-	num2_add = int(input('num2 > '))
-	answer_add = (num1_add + num2_add)
-	print(answer_add)
-elif operation == 's':
-	num1_subtract = int(input('num1 > '))
-	num2_subtract = int(input('num2 > '))
-	answer_subtract = (num1_subtract - num2_subtract)
-	print(answer_subtract)
-elif operation == 'm':
-	num1_multiply = int(input('num1 > '))
-	num2_multiply = int(input('num2 > '))
-	answer_multiply = (num1_multiply * num2_multiply)
-	print(answer_multiply)
-elif operation == 'd':
-	num1_divide = int(input('num1 > '))
-	num2_divide = int(input('num2 > '))
-	answer_divide = (num1_divide / num2_divide)
-	print(answer_divide)
-elif operation == 'mo':
-	num1_modulo = int(input('num1 > '))
-	num2_modulo = int(input('num2 > '))
-	answer_modulo = (num1_modulo % num2_modulo)
-	print(answer_modulo)
-elif operation == 'i':
-	num1_int = int(input('num1 > '))
-	num2_int = int(input('num2 > '))
-	answer_int = (num1_int // num2_int)
-	print(answer_int)
-elif operation == 'q':
-	exit(120)
-else:
-	print('Give valid command')
+	clear()
+	if operation == 'add':
+		num1_add = float(input('num1 > '))
+		num2_add = float(input('num2 > '))
+		answer_add = (num1_add + num2_add)
+		print(colored(answer_add, 'green'))
+		sleep(5)
+		clear()
+	elif operation == 'subtract':
+		num1_subtract = float(input('num1 > '))
+		num2_subtract = float(input('num2 > '))
+		answer_subtract = (num1_subtract - num2_subtract)
+		print(colored(answer_subtract, 'green'))
+	elif operation == 'multiply':
+		num1_multiply = float(input('num1 > '))
+		num2_multiply = float(input('num2 > '))
+		answer_multiply = (num1_multiply * num2_multiply)
+		print(colored(answer_multiply, 'green'))
+		sleep(5)
+		clear()
+	elif operation == 'divide':
+		num1_divide = float(input('num1 > '))
+		num2_divide = float(input('num2 > '))
+		answer_divide = (num1_divide / num2_divide)
+		print(colored(answer_divide, 'green'))
+		sleep(5)
+		clear()
+	elif operation == 'modulo':
+		num1_modulo = float(input('num1 > '))
+		num2_modulo = float(input('num2 > '))
+		answer_modulo = (num1_modulo % num2_modulo)
+		print(answer_modulo)
+		sleep(5)
+		clear()
+	elif operation == 'integer division':
+		num1_int = float(input('num1 > '))
+		num2_int = float(input('num2 > '))
+		answer_int = (num1_int // num2_int)
+		print(colored(answer_int, "green"))
+		sleep(5)
+		clear()
+	elif operation == 'power of':
+		num1_pf = float(input('num1 > '))
+		num2_pf = float(input('num2 > '))
+		print(colored(num1_pf ** num2_pf, "green"))
+		sleep(5)
+		clear()
+	elif operation == 'square root':
+		num_sqrt = float(input('num > '))
+		print('approx.', colored(round(math.sqrt(num_sqrt), 2), "green"))
+		sleep(5)
+		clear()
+	elif operation == 'expressions':
+		exp_ops = input('''~~~OPTIONS~~~
+[equality]
+-----MORE TO BE ADDED-----
+>>> ''')
+		if exp_ops == 'equality':
+			exp1 = int(input('num1 > '))
+			exp2 = int(input('num2 > '))
+			if exp1 == exp2:
+				print(colored('True!', 'green'))
+				sleep(5)
+				clear()
+			else:
+				print(colored('False!', "red"))
+				sleep(5)
+				clear()
+	elif operation == 'quit':
+		exit(120)
+	else:
+		print(colored('Give valid command', 'red'))
+		sleep(5)
+		clear()
